b062c5b1cc46d987f97a858079ff20c8dc71c248
GableS-projects <111135912+GableS-projects@users.noreply.github.com>
2026-01-01 23:16:23 -0500

    Add files via upload

---
+++ intermediate_calculations/combine_results.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import glob
+
+
+# In[2]:
+
+
+input_list = glob.glob('/nfs/turbo/lsa-yiheh/yiheh-mistorage/gablesyd/PermianBasinData/MB04_results/*')
+cc_thresh = 
+output_file = 
+
+
+# In[3]:
+
+
+frame = pd.read_csv(input_list[0])
+
+file_list = [None]*len(input_list)
+count = 1
+
+for i in range(len(input_list)):
+    
+    frame = pd.read_csv(input_list[i])
+    
+    frame = frame[frame['cc'] > cc_thresh]
+    
+    file_list[i] = frame
+    #print(frame.shape[0], str(count)+' of '+ str(len(input_list)))
+    
+    count += 1
+
+
+# In[4]:
+
+
+df = pd.concat(file_list, axis = 0, ignore_index = True)
+
+aggregation_functions = {'cc':'mean', 'AR':'mean', 'cha':list}
+df_new = df.groupby(['dtm', 'dtn'], as_index=False).aggregate(aggregation_functions).reindex(columns=df.columns)
+
+
+# In[5]:
+
+
+df_new.to_csv(output_file)
+
+
+# In[ ]:
+
+
+
+
